0caf263856f9f0f858f49e12699c78ed898bb640
ciuccislab <57649983+ciuccislab@users.noreply.github.com>
2023-09-11 01:49:12 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setuptools.setup(
         "console_scripts": [
             "launch_pydrttool=src.pyDRTtools_GUI:main",
         ],
+    },
     classifiers = [
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -30,14 +31,14 @@ setuptools.setup(
     ],
     install_requires=[
         # List your dependencies here
-        "cvxopt == 1.3.2",  
-        "cvxpy == 1.3.2",
-        "matplotlib == 3.7.2",
-        "pandas == 2.0.3",
-        "numpy == 1.25.2",
-        "scipy == 1.11.2",
-        "scikit-learn == 1.3.0",
-        "PyQt5 == 5.15.9",
+        "cvxopt ~= 1.3.2",  
+        "cvxpy ~= 1.3.2",
+        "matplotlib ~= 3.7.2",
+        "pandas ~= 2.0.3",
+        "numpy ~= 1.25.2",
+        "scipy ~= 1.11.2",
+        "scikit-learn ~= 1.3.0",
+        "PyQt5 ~= 5.15.9",
     ],
     package_dir = {"": "src"},
     packages = setuptools.find_packages(where="src"),
