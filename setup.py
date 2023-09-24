d77b39abe30fcb14d159a6c125b0618149b40ffc
ciuccislab <57649983+ciuccislab@users.noreply.github.com>
2023-09-25 04:38:46 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ entry_points={
 dependencies = [
     "cvxopt~=1.3",  # cvxopt optimizer
     "cvxpy>=1.0.3.2",
-    "requests~=2.28",  # Used to check package status on PyPI.
+    "requests~=2.28",  
     "scipy==1.10.0",
     "numpy==1.24.1",
     "scikit-learn>=1.2.1",
@@ -25,25 +25,9 @@ dependencies = [
     "pandas==1.5.3",
 ]
 
-dev_dependencies = [
-    "build~=0.10",
-    "flake8~=6.0",
-]
-
-optional_dependencies = {  
-    "kvxopt": "kvxopt~=1.3",  
-    "dev": dev_dependencies,
-}
-
-if __name__ == "__main__":
-    with open("requirements.txt", "w") as fp:
-        fp.write("\n".join(dependencies))
-    with open("dev-requirements.txt", "w") as fp:
-        fp.write("\n".join(dev_dependencies))
-        
-    setuptools.setup(
+setuptools.setup(
     name = "pyDRTtools",
-    version = "0.2.8.66",
+    version = "0.2.8.67",
     author = "ciuccislab",
     author_email = "amaradesa@connect.ust.hk",
     description = "pyDRTtools: A Python-based DRTtools to Deconvolve the Distribution of Relaxation Times from Electrochemical Impedance Spectroscopy Data",
@@ -60,8 +44,8 @@ if __name__ == "__main__":
         "Bug Tracker": "https://github.com/ciuccislab/pyDRTtools/issues",
     },
     entry_points=entry_points,
-    install_requires=dependencies,
-    extras_require=optional_dependencies,
+    #install_requires=dependencies,
+    #extras_require=optional_dependencies,
     python_requires = ">=3.4",
     
     classifiers = [
@@ -80,45 +64,3 @@ if __name__ == "__main__":
           
     ],
 )
-
-
-
-# setuptools.setup(
-#     name = "pyDRTtools",
-#     version = "0.2.8.63",
-#     author = "ciuccislab",
-#     author_email = "amaradesa@connect.ust.hk",
-#     description = "pyDRTtools: A Python-based DRTtools to Deconvolve the Distribution of Relaxation Times from Electrochemical Impedance Spectroscopy Data",
-#     long_description = readme(),
-#     long_description_content_type = "text/markdown",
-#     ###
-#     packages = setuptools.find_packages(where="src"),
-#     package_dir={"": "src"},
-#     include_package_data=True,
-#     ###
-#     url = "https://github.com/ciuccislab/pyDRTtools",
-#     project_urls = {
-#         "Source Code": "https://github.com/ciuccislab/pyDRTtools",
-#         "Bug Tracker": "https://github.com/ciuccislab/pyDRTtools/issues",
-#     },
-#     entry_points=entry_points,
-#     install_requires=dependencies,
-#     extras_require=optional_dependencies,
-#     python_requires = ">=3.4",
-    
-#     classifiers = [
-#         "Operating System :: POSIX :: Linux",
-#         "Operating System :: Microsoft :: Windows",
-#         "Operating System :: MacOS",
-#         "Programming Language :: Python :: 3.4",
-#         "Programming Language :: Python :: 3.5",
-#         "Programming Language :: Python :: 3.6",
-#         "Programming Language :: Python :: 3.7",
-#         "Programming Language :: Python :: 3.8",
-#         "Programming Language :: Python :: 3.9",
-#         "Programming Language :: Python :: 3.10",
-#         "License :: OSI Approved :: MIT License",
-#         #"Operating System :: OS Independent",
-          
-#     ],
-# )
