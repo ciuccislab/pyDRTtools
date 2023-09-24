ad8613606dd47661bd7de708ec8a71786343c819
ciuccislab <57649983+ciuccislab@users.noreply.github.com>
2023-09-24 21:05:43 +0800

    Update pyDRTtools_BHT.py

--- src/pyDRTtools/pyDRTtools_BHT.py
+++ src/pyDRTtools/pyDRTtools_BHT.py
@@ -10,7 +10,7 @@ from scipy.optimize import minimize
 from scipy.stats import multivariate_normal as MVN
 
 # DRTtools related package
-from . import pyDRTtools_basics as gf
+import pyDRTtools_basics as gf
 
 
 """
