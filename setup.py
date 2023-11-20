4530198b9730040fcb24755cb1d53dc6e02cef0a
ciuccislab <57649983+ciuccislab@users.noreply.github.com>
2023-11-20 22:19:42 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ dependencies = [
 
 setuptools.setup(
     name = "pyDRTtools",
-    version = "0.2.0.1",
+    version = "0.2.0.80",
     author = "ciuccislab",
     author_email = "amaradesa@connect.ust.hk",
     description = "pyDRTtools: A Python-based DRTtools to Deconvolve the Distribution of Relaxation Times from Electrochemical Impedance Spectroscopy Data",
