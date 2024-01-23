eb998da654153ea57790685404565c28c0c69afe
ciuccislab <57649983+ciuccislab@users.noreply.github.com>
2024-01-23 12:55:52 +0800

    Update GUI.py

--- pyDRTtools/GUI.py
+++ pyDRTtools/GUI.py
@@ -213,6 +213,7 @@ class GUI(QtWidgets.QMainWindow):
                 # first save L and R
                 writer.writerow(['L', self.data.L])
                 writer.writerow(['R', self.data.R])
+                writer.writerow(['lambda_value', self.data.lambda_value])
                 writer.writerow(['tau','gamma'])
                 
                 # after that, save tau and gamma
